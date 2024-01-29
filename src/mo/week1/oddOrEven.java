c7ee771c7e3db2130331655e1462043bb720fc18
mohamedbourazza <mohamed.bourazza1@gmail.com>
2024-01-29 11:53:34 -0500

    working on the tasks

---
+++ src/mo/week1/oddOrEven.java
@@ -0,0 +1,9 @@
+package mo.week1;
+
+public class oddOrEven {
+    /*Write  a method which can identify given number is even or odd
+    Output ex:
+    identify(5); ->"Odd"
+    identify(6); ->"Even"*/
+
+}

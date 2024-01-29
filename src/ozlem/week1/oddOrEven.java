db8452b56d0d6c4a95c45eb65a29d58a46ae3d7d
Ozlem Sozer <ozlemsozer06@gmail.com>
2024-01-29 15:00:53 -0500

    week 1 added some codes

--- src/ozlem/week1/oddOrEven.java
+++ src/ozlem/week1/oddOrEven.java
@@ -7,5 +7,13 @@ public class oddOrEven {
     identify(6); ->"Even"*/
     public static void main(String[] args) {
 
+       int num = 0;
+
+        if (num%2==0){
+            System.out.println(num + " is even number");
+        }else {
+            System.out.println(num + "is odd number");
+        }
+
     }
 }

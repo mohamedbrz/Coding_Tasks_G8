69425e191028c6d466a90dcedb424c3b451804db
mohamedbourazza <mohamed.bourazza1@gmail.com>
2024-01-29 12:07:27 -0500

    oddOrEven solved

--- src/mo/week1/oddOrEven.java
+++ src/mo/week1/oddOrEven.java
@@ -6,4 +6,16 @@ public class oddOrEven {
     identify(5); ->"Odd"
     identify(6); ->"Even"*/
 
+    public static void EvenOdd (int num){
+        if (num % 2 ==0 ){
+            System.out.println(num + " is Even");
+        }else{
+            System.out.println(num + " is Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+      EvenOdd(99);
+
+    }
 }

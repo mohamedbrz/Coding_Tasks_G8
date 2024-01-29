3ae82e1c8fb8be87064f0939772d11b7f434ee89
Ozlem Sozer <ozlemsozer06@gmail.com>
2024-01-29 15:00:53 -0500

    week 1 added some codes

--- src/ozlem/week1/divisionWithoutOperator.java
+++ src/ozlem/week1/divisionWithoutOperator.java
@@ -4,5 +4,8 @@ public class divisionWithoutOperator {
     //Write a method that can divide two numbers without using division operator
     public static void main(String[] args) {
 
+
+
+        }
     }
-}
+

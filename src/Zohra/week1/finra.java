02636e21083af16cb421dac3bc333f959629e19e
zohraghafoory <zohraghafoory86@gmail.com>
2024-01-29 22:16:30 -0500

    Created printFinra method

--- src/Zohra/week1/finra.java
+++ src/Zohra/week1/finra.java
@@ -6,4 +6,18 @@ public class finra {
     //for numbers which are a multiple of 5, print "RA" instead of the number.
     //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
 
-}
+    public static void printFinra(){
+        for (int i = 1; i < 30; i++) {
+            if(i % 3 ==0&& i % 5 ==0){
+                System.out.println("FINRA");
+            } else if (i % 3 == 0 ) {
+                System.out.println("FIN");
+            }else if(i % 5 ==0) {
+                System.out.println("RA");
+            }else {
+                System.out.println(i);
+            }
+        }
+
+
+}}

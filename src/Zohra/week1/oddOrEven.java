217dde954cc638aff95d1fac5fe9aff6e8dba3f2
zohraghafoory <zohraghafoory86@gmail.com>
2024-01-29 22:08:36 -0500

    Created oddOrEven method

--- src/Zohra/week1/oddOrEven.java
+++ src/Zohra/week1/oddOrEven.java
@@ -6,4 +6,13 @@ public class oddOrEven {
     identify(5); ->"Odd"
     identify(6); ->"Even"*/
 
+    public static void isEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        }else{
+            System.out.println(num+ " is odd");
+        }
+
+    }
 }
+

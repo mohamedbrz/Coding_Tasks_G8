817f4b0988e1d5057b2672f26b848ccd8f81f435
cemilaybaci <cemilaybaci@gmail.com>
2024-01-30 21:29:29 +0300

    Made a method to divide 2 numbers without using division operator

--- src/Cemil/week1/divisionWithoutOperator.java
+++ src/Cemil/week1/divisionWithoutOperator.java
@@ -3,4 +3,22 @@ package Cemil.week1;
 public class divisionWithoutOperator {
     //Write a method that can divide two numbers without using division operator
 
+    public static void main(String[] args) {
+        divide(6,3);
+        divide(25,4);
+        divide(36,8);
+    }
+
+    public static void divide(int num1, int num2){
+        int count = 0;
+
+        while (num1 >= num2){
+
+            num1 -= num2;
+            count++;
+
+        }
+        System.out.println(count + " with a remainder of " + num1);
+    }
+
 }

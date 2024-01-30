d589dfe945053f5d676c98d597a81f10e6ff324a
zohraghafoory <zohraghafoory86@gmail.com>
2024-01-29 22:00:16 -0500

    Created divideWithoutOperator method

--- src/Zohra/week1/divisionWithoutOperator.java
+++ src/Zohra/week1/divisionWithoutOperator.java
@@ -3,4 +3,24 @@ package Zohra.week1;
 public class divisionWithoutOperator {
     //Write a method that can divide two numbers without using division operator
 
-}
+    public static void divideWithoutOperator(int d1, int d2){
+
+        if(d2==0){
+            System.out.println("Cannot divide by zero");
+            return;
+        }
+
+        int result=0;
+        while(d1 >=d2){
+            d1-=d2;
+            result++;
+        }
+
+        System.out.println(result);
+
+    }}
+
+
+
+
+

6dd4f3c3ce262f26a372b0e45a208d6d3299b0c4
Ozlem Sozer <ozlemsozer06@gmail.com>
2024-01-30 19:24:19 -0500

    added solution with explanation

--- src/ozlem/week1/finra.java
+++ src/ozlem/week1/finra.java
@@ -22,5 +22,26 @@ public class finra {
     }
 }
 
+/*EXPLANATION
+Loop (for loop):
 
+The loop runs from num = 1 to num = 30, incrementing num by 1 in each iteration.
+Conditions:
+
+It checks three conditions for each number:
+If the number is a multiple of both 3 and 5, it prints "FINRA."
+If the number is only a multiple of 3, it prints "FIN."
+If the number is only a multiple of 5, it prints "RA."
+If the number doesn't meet any of these conditions, it prints the number itself.
+Printing:
+
+The System.out.println() statements print the appropriate message or number based on the conditions.
+So, when you run this program, it will print numbers from 1 to 30, replacing multiples of 3 with "FIN," multiples of 5 with "RA," and multiples of both 3 and 5 with "FINRA."
+
+
+
+
+
+
+*/
 
--- src/ozlem/week1/oddOrEven.java
+++ src/ozlem/week1/oddOrEven.java
@@ -17,3 +17,25 @@ public class oddOrEven {
 
     }
 }
+/*Explanation:
+
+Number to Check (int num = 0;):
+
+You have a variable num initialized to 0. This is the number you want to check whether it's even or odd.
+Checking Even or Odd:
+
+The if statement checks if the number is even by using the condition num % 2 == 0.
+If the condition is true, it prints that the number is even.
+If the condition is false (meaning the number is odd), it prints that the number is odd.
+Printing Result:
+
+System.out.println() statements are used to print whether the number is even or odd.
+So, when you run this program, it will output whether the given number (num) is even or odd. In your specific code, since num is initialized to 0, the output will be "0 is an even number." If you want to check other numbers, you can change the value of num accordingly.
+
+
+
+
+
+
+
+*/

a92ce329f46941fb7cc5196b843deecb4a264999
aaronqa <aaron@cybertekschool.com>
2024-02-01 18:21:59 +0300

    week 2 tasks folder structure corrected

--- src/questions/oddOrEven.java
+++
@@ -1,9 +0,0 @@
-package questions;
-
-public class oddOrEven {
-    /*Write  a method which can identify given number is even or odd
-    Output ex:
-    identify(5); ->"Odd"
-    identify(6); ->"Even"*/
-
-}

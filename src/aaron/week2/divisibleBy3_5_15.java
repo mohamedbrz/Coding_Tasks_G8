54adb9b4ca12b8e354d17b627f3e5d0686fe31ad
aaronqa <aaron@cybertekschool.com>
2024-02-01 18:50:23 +0300

    week 2 tasks solved

--- src/aaron/week2/divisibleBy3_5_15.java
+++ src/aaron/week2/divisibleBy3_5_15.java
@@ -11,8 +11,40 @@ Divisible By 15 15 30 45 60 75 90
 Divisible By 5 5 10 20 25 35 40 50 55 65 70 80 85 95 100
 Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72 78 81 84 87 93 96 99
 */
+public static void main(String[] args) {
+    solution(100);
+}
+    public static void solution(int n) {
+        String divisibleBy15 = "";
+        String divisibleBy5 = "";
+        String divisibleBy3 = "";
 
+        int[] arr = new int[n];
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int each : arr) {
+
+            if (each % 15 == 0 && each % 3 == 0) {
+                divisibleBy15 += each + " ";
+            }
+
+            if (each % 5 == 0 && each % 15 != 0) {
+                divisibleBy5 += each + " ";
+            }
+
+            if (each % 3 == 0 && each % 15 != 0) {
+                divisibleBy3 += each + " ";
+            }
+
+        }
+
+        System.out.println("Divisible By 15: " + divisibleBy15);
+        System.out.println("Divisible By 5: " + divisibleBy5);
+        System.out.println("Divisible By 3: " + divisibleBy3);
+    }
 
 }
 
--- src/aaron/week2/printConsecutiveNumbers.java
+++ src/aaron/week2/printConsecutiveNumbers.java
@@ -1,6 +1,7 @@
 package aaron.week2;
 public class printConsecutiveNumbers {
-         /*  Write a function: that, given a positive integer N, prints the consecutive numbers from 1 to N,
+    public static void main(String[] args) {
+      /*  Write a function: that, given a positive integer N, prints the consecutive numbers from 1 to N,
       each on a separate line. However, any number divisible by 2, 3 or 5 should be replaced by the word Codility,
       Test or Coders respectively. If a number is divisible by more than one of the numbers: 2,3 or 5,
       it should be replaced by a concatenation of the respective words Codility, Test and Coders in this given order.
@@ -10,7 +11,7 @@ public class printConsecutiveNumbers {
         For example, here is the output for N = 24:
         1
         Codility
-        Test
+       Test
         Codility
         Coders
         CodilityTest
@@ -26,5 +27,31 @@ public class printConsecutiveNumbers {
         Codility
         17
 */
+        solution(15);
 
+    }
+
+    //Solution:
+    public static void solution(int N) {
+        String result = "";
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
+                result += "CodilityTestCoders\n";
+            else if (i % 2 == 0 && i % 3 == 0)
+                result += "CodilityTest\n";
+            else if (i % 2 == 0 && i % 5 == 0)
+                result += "CodilityCoders\n";
+            else if (i % 3 == 0 && i % 5 == 0)
+                result += "TestCoders\n";
+            else if (i % 2 == 0)
+                result += "Codility\n";
+            else if (i % 5 == 0)
+                result += "Coders\n";
+            else if (i % 3 == 0)
+                result += "Test\n";
+            else
+                result += i + "\n";
+        }
+        System.out.println(result);
+    }
 }

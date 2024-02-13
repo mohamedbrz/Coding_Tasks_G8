21ef39de20c1d6064e45d21772819ab33cbf4578
ZohraG <zohraghafoory86@gmail.com>
2024-02-13 12:36:00 -0500

    Added primeNumber & reverseNegativeNumber

---
+++ src/Zohra/week3/reverseNegativeNumber.java
@@ -0,0 +1,10 @@
+package Zohra.week3;
+
+public class reverseNegativeNumber {
+    //Write a return method that can reverse digits of a negative number and return it as int
+    //input: -35  output: -53
+
+
+
+}
+

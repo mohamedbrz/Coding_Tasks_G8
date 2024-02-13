80a34b2d0519be70de3383f24c0657d1bd8b3216
dkamin212 <dkamin212@gmail.com>
2024-02-13 09:04:52 -0500

    solve week 3 tasks

--- src/daniel/week3/primeNumber.java
+++ src/daniel/week3/primeNumber.java
@@ -3,7 +3,19 @@ package daniel.week3;
 public class primeNumber {
      // Write a method that can check if a number is prime or not
 
+    public static void main(String[] args) {
+        System.out.println(isPrime(97));
+    }
 
+    public static boolean isPrime(int num){
+
+        for (int i = 2; i < 100; i++) {
+            if (num % i == 0 && i != num){
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 }

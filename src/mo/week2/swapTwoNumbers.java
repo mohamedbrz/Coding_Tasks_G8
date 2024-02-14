3ee8cfb3c067cdd0c63168f802b4ba1ccbf109b1
mohamedbrz <mohamed.bourazza1@gmail.com>
2024-02-14 11:58:30 -0500

    Swap two number update

--- src/mo/week2/swapTwoNumbers.java
+++ src/mo/week2/swapTwoNumbers.java
@@ -15,7 +15,7 @@ public class swapTwoNumbers {
             // swap numbers
             x = x + y; // 10 + 20 = 30
             y = x - y; // 30 - 20 = 10
-            x = x - y; // 30 - 10 = 20
+       x = x - y; // 30 - 10 = 20
 
             System.out.println("x= " + x + " " + "y= " + y);
 

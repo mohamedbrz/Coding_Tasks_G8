c3fbbc232dd4fc910ac4a3cc44f390b7f97a75a2
mohamedbrz <mohamed.bourazza1@gmail.com>
2024-02-19 12:03:53 -0500

    week 3 & week 4 tasks added

---
+++ src/mo/week4/RemoveDuplicates.java
@@ -0,0 +1,8 @@
+package mo.week4;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
+
+}

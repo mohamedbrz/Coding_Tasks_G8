877378123c8123a731a3b0f141e59b52855b7e23
Aaron <aaron@cybertekschool.com>
2024-02-20 20:25:31 +0300

    Week 5 questions

---
+++ src/aaron/week5/findUnique.java
@@ -0,0 +1,8 @@
+package aaron.week5;
+
+public class findUnique {
+    /*Write a return  method that can find the unique characters from the String
+      Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";*/
+
+
+}

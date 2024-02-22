02ac10698c5b20b30589813006897b1e1f2b4f20
Aaron <aaron@cybertekschool.com>
2024-02-22 17:47:35 +0300

    removeduplicates fixed

--- src/aaron/week4/RemoveDuplicates.java
+++ src/aaron/week4/RemoveDuplicates.java
@@ -3,32 +3,21 @@ package aaron.week4;
 public class RemoveDuplicates {
     /*Write a return method that can remove the duplicated values from String
         Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
     public static void main(String[] args) {
-
-
-        String str = "aaabbbccddff";
-        String result = "";
-
-        for (int j = 0; j < str.length(); j++) {
-            char ch = str.charAt(j); //a a a b b
-            int count = 0;
-
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == ch) {
-                    count++;
-                }
-            }
-
-            if (result.contains(ch + "")) {
-                continue;
-            }
-            result += ch + "" + count;
-
-        }
-
-        System.out.println(result);
-
+        System.out.println(removeDup("aaabbbccc"));
 
     }
-}
 
+    public static String removeDup(String str) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++)
+
+            if (!result.contains("" + str.charAt(i)))
+
+                result += "" + str.charAt(i);
+        return result;
+    }
+}

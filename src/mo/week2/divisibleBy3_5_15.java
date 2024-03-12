3d132b51de689a814c638b340985a653c2594947
mohamedbrz <mohamed.bourazza1@gmail.com>
2024-03-12 12:11:59 -0400

    tasks solved

--- src/mo/week2/divisibleBy3_5_15.java
+++ src/mo/week2/divisibleBy3_5_15.java
@@ -22,6 +22,7 @@ public static void main(String[] args) {
 
         for (int i = 1; i <= num; i++) {
 
+
             if (i % 15 == 0) {
                 by15 += i + " ";
             } else if (i % 5 == 0) {

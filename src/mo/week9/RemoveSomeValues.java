30311d8a869d360134d1e1ba397457f9cccea398
mohamedbrz <mohamed.bourazza1@gmail.com>
2024-04-03 12:03:08 -0400

    WEEK 9

---
+++ src/mo/week9/RemoveSomeValues.java
@@ -0,0 +1,9 @@
+package mo.week9;
+
+
+public class RemoveSomeValues {
+    //Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
+
+
+
+}
---
+++ src/mo/week9/concat2Arrays.java
@@ -0,0 +1,12 @@
+package mo.week9;
+
+
+import java.util.Arrays;
+
+
+public class concat2Arrays {
+    //Write a return method that can concatenate two arrays
+
+
+
+}

e935838d8440ca9b1188a5ec8da2acc507e41a15
Aaron <aaron@cybertekschool.com>
2024-04-11 15:45:11 +0300

    Week 11 Tasks Published

---
+++ src/questions/week11/mapFrequencyChars.java
@@ -0,0 +1,11 @@
+package questions.week11;
+
+
+public class mapFrequencyChars {
+//Write a method that prints the frequency of each character from a String
+
+
+
+
+
+}
---
+++ src/questions/week11/mapMinValue.java
@@ -0,0 +1,11 @@
+package questions.week11;
+
+
+public class mapMinValue {
+//Write a method that can return the minimum value from a map (DO NOT use sort method)
+
+
+
+
+
+}
